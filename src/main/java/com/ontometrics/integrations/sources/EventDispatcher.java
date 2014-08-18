3158a2580d45a6077e145ec0e30c732c86210617
rob <rob@ontometrics.com>
2014-08-18 11:15:21 -0700

    Rebuilt the project to get rid of Akka/Play.

---
+++ src/main/java/com/ontometrics/integrations/sources/EventDispatcher.java
@@ -0,0 +1,16 @@
+package com.ontometrics.integrations.sources;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+/**
+ * Created by rob on 7/17/14.
+ * Copyright (c) ontometrics, 2014 All Rights Reserved
+ */
+public class EventDispatcher {
+
+    public void postEvent(ProcessEvent event, String channel){
+        Client client = ClientBuilder.newClient();
+    }
+
+}
