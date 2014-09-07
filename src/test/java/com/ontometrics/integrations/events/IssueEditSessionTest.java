0bd6aac677206a05d4306a18d02e01be95281daa
rob <rob@ontometrics.com>
2014-09-07 14:39:55 -0700

    AIA-45 any issue where updated and created are w/in 5 minutes of each other, given no other changes.

--- src/test/java/com/ontometrics/integrations/events/IssueEditSessionTest.java
+++ src/test/java/com/ontometrics/integrations/events/IssueEditSessionTest.java
@@ -3,9 +3,12 @@ package com.ontometrics.integrations.events;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
+
+import com.ontometrics.util.DateBuilder;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
+import java.util.Date;
 
 public class IssueEditSessionTest {
 
@@ -17,6 +20,16 @@ public class IssueEditSessionTest {
         assertThat(issueEditSession.getChanges(), hasSize(2));
         assertThat(issueEditSession.getUpdater(), is("Noura"));
 
+    }
+
+    @Test
+    public void canDetectNewTickets() {
+        Date justNow = new Date();
+        Date threeMinutesAgo = new DateBuilder().start(justNow).addMinutes(-3).build();
+        Issue issue = new Issue.Builder().projectPrefix("AIA").id(721).created(threeMinutesAgo).build();
+        IssueEditSession issueEditSession = new IssueEditSession.Builder().issue(issue).updated(justNow).build();
+
+        assertThat(issueEditSession.isCreationEdit(), is(true));
     }
 
 }
