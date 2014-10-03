e960c5c40bfd68702b6d628a70ae66c316313538
Rob Williams <rob@ontometrics.com>
2014-10-03 08:23:45 -0700

    Adding channel mapping for agent portal project. Need to get this out of the compilation side.

---
+++ src/test/java/ontometrics/test/util/ExtractionStreamWrapperTests.java
@@ -0,0 +1,19 @@
+package ontometrics.test.util;
+
+/**
+ * User: Rob
+ * Date: 9/11/14
+ * Time: 8:49 PM
+ * <p/>
+ * (c) ontometrics 2014, All Rights Reserved
+ */
+public class ExtractionStreamWrapperTests {
+
+
+
+
+}
+
+class Extractor {
+
+}
